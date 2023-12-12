69796e601d3115da7fcb96680c9e607013695e11
jsssi <jeffesilva546@gmail.com>
2023-12-11 22:18:04 -0300

    mudança na api

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-const url = 'https://currency-converter18.p.rapidapi.com/api/v1/convert?from=EUR&to=KWD&amount=10';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c014cfedbbmsh41c94c81cd7309ap14c406jsn5fbbb386bd4c',
-		'X-RapidAPI-Host': 'currency-converter18.p.rapidapi.com'
+class sla {
+	static search(from, to) {
+		const url = `https://currency-converter18.p.rapidapi.com/api/v1/convert?from=${from}&to=${to}&amount=1`;
+		const key = 'c014cfedbbmsh41c94c81cd7309ap14c406jsn5fbbb386bd4c'
+		const options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': key,
+				'X-RapidAPI-Host': 'currency-converter18.p.rapidapi.com'
+			}
+		};
+		return fetch(url,options).then(result => result.json())
 	}
-};
-fetch(url,options).then(result => result.json()).then(({result:{to,from}}) =>{
- to,
- from
+} 
+
+sla.search('EUR','BRL').then(data =>{
+	console.log(data.result.convertedAmount)
+	alert('sai')
 })
 
